2b8453febbf92bc3bfedd59059e5a1d2e741a46f
shw <shw20010329@163.com>
2025-10-22 02:40:51 +0000

    fix ffa_utils check

--- magi_attention/functional/_flex_flash_attn_jit.py
+++ magi_attention/functional/_flex_flash_attn_jit.py
@@ -28,8 +28,9 @@ from magi_attention.common.jit.utils import write_if_different
 # We need to import the CUDA kernels after importing torch
 is_ffa_utils_installed = False
 try:
-    is_ffa_utils_installed = True
     from magi_attention import flexible_flash_attention_utils_cuda  # type: ignore[attr-defined]
+
+    is_ffa_utils_installed = True
 except ImportError:
     pass
 
@@ -240,11 +241,12 @@ def get_ffa_jit_spec(
         )
 
         common_objects = common_spec.build_and_get_objects()
-        if flexible_flash_attention_utils_cuda is False:
-            raise ImportError(
-                "The 'flexible_flash_attention_utils_cuda' module could not be imported."
-                "This is a required dependency for JIT compilation. "
-            )
+
+        assert is_ffa_utils_installed, (
+            "The `flexible_flash_attention_utils_cuda` "
+            "extension module is not installed."
+            "This is a required dependency for JIT compilation."
+        )
         # add utils.so(Dynamic linking)
         utils_so_path = Path(flexible_flash_attention_utils_cuda.__file__)
 
