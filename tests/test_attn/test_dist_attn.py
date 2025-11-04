6576bebf55c72ee3637a59895534d053806521d5
Strivin0311 <hyp@smail.nju.edu.cn>
2025-11-04 11:46:52 +0000

    enhanced the test_dist_attn

--- tests/test_attn/test_dist_attn.py
+++ tests/test_attn/test_dist_attn.py
@@ -12,22 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from functools import partial
+
 import torch
 import torch.distributed as dist
-from einops import rearrange
 from torch.distributed.device_mesh import init_device_mesh
 from torch.distributed.nn.functional import all_gather
-from torch.nn.functional import scaled_dot_product_attention
 from torch.testing._internal.common_distributed import skip_if_lt_x_gpu
 from torch.testing._internal.common_utils import run_tests
 
 import magi_attention
+from magi_attention.comm.primitive.grpcoll._config import GrpCollConfig
+from magi_attention.comm.primitive.grpcoll._mgr import grpcoll_mgr
 from magi_attention.common.ranges import AttnRanges
 from magi_attention.functional.dist_attn import DistAttnRuntime, dist_attn_func
 from magi_attention.meta.collection.calc_meta import AttnArg, CalcMeta
 from magi_attention.meta.collection.comm_meta import CommMeta, GroupCollectiveArg
 from magi_attention.testing import parameterize
 from magi_attention.testing.dist_common import DistTestBase, with_comms
+from magi_attention.testing.precision import EPSILON, assert_close, ref_attn_func
+from magi_attention.testing.utils import switch_envvar_context
 
 
 # TODO: add more unitest for dist attn
@@ -54,6 +58,32 @@ class TestDistAttn(DistTestBase):
         else:
             self.device_mesh = None
 
+        # -----    set up for native grpcoll   ---- #
+
+        self._switch_native_grpcoll_context = partial(
+            switch_envvar_context, envvar_name="MAGI_ATTENTION_NATIVE_GRPCOLL"
+        )
+
+        grpcoll_mgr.register_buffer(
+            group=self.nccl_group,
+            config=GrpCollConfig(
+                num_sms=24,
+                nvl_chunk_size=8,
+                nvl_buffer_size=256,
+                rdma_chunk_size=8,
+                rdma_buffer_size=256,
+                num_nvl_bytes=int(1e9),
+                num_rdma_bytes=0,
+            ),
+        )
+        grpcoll_mgr.check_registered(group=self.nccl_group)
+
+    def destroy_pg(self):
+        grpcoll_mgr.release_buffer(group=self.nccl_group)
+        grpcoll_mgr.check_released(group=self.nccl_group)
+
+        super().destroy_pg()
+
     @property
     def nccl_group(self) -> dist.ProcessGroup:
         return self.nccl_groups[0]
@@ -68,6 +98,8 @@ class TestDistAttn(DistTestBase):
 
     @skip_if_lt_x_gpu(4)
     @with_comms
+    @parameterize("seqlen_sink", [0])
+    @parameterize("use_native_grpcoll", [False, True])
     @parameterize(
         "dtype",
         [
@@ -75,103 +107,183 @@ class TestDistAttn(DistTestBase):
             torch.bfloat16,
         ],
     )
-    def test_full_attn(self, dtype):
-        device = torch.cuda.current_device()
+    def test_full_attn(
+        self, seqlen_sink: int, use_native_grpcoll: bool, dtype: torch.dtype
+    ):
+        with self._switch_native_grpcoll_context(enable=use_native_grpcoll):
+            device = torch.cuda.current_device()
 
-        calc_meta = CalcMeta(
-            local_attn_arg=AttnArg(
-                q_ranges=AttnRanges.from_ranges([[0, 128]]),
-                k_ranges=AttnRanges.from_ranges([[0, 128]]),
-                attn_type_map=[0],
-                total_area=128 * 128,
-            ),
-            remote_attn_args_list=[
-                AttnArg(
+            calc_meta = CalcMeta(
+                local_attn_arg=AttnArg(
                     q_ranges=AttnRanges.from_ranges([[0, 128]]),
-                    k_ranges=AttnRanges.from_ranges([[0, 128 * 3]]),
+                    k_ranges=AttnRanges.from_ranges([[0, 128]]),
                     attn_type_map=[0],
-                    total_area=128 * 128 * 3,
+                    total_area=128 * 128,
                 ),
-            ],
-        )
+                remote_attn_args_list=[
+                    AttnArg(
+                        q_ranges=AttnRanges.from_ranges([[0, 128]]),
+                        k_ranges=AttnRanges.from_ranges([[0, 128 * 3]]),
+                        attn_type_map=[0],
+                        total_area=128 * 128 * 3,
+                    ),
+                ],
+            )
 
-        comm_meta = CommMeta(
-            num_remote_kv_tokens_per_stage=[128 * 3],
-            kv_group_collective_args_list=[
-                GroupCollectiveArg(
-                    input_split_size_list=[128],
-                    output_split_size_list=[128, 128, 128],
-                    dst_indices_list=[
-                        [rank for rank in range(self.world_size) if rank != self.rank]
-                    ],
-                    src_index_list=[
-                        rank for rank in range(self.world_size) if rank != self.rank
-                    ],
-                    rank=self.rank,
-                    world_size=self.world_size,
-                    group=self.nccl_group,
-                    device_mesh=self.device_mesh,
+            comm_meta = CommMeta(
+                num_remote_kv_tokens_per_stage=[128 * 3],
+                kv_group_collective_args_list=[
+                    GroupCollectiveArg(
+                        input_split_size_list=[128],
+                        output_split_size_list=[128, 128, 128],
+                        dst_indices_list=[
+                            [
+                                rank
+                                for rank in range(self.world_size)
+                                if rank != self.rank
+                            ]
+                        ],
+                        src_index_list=[
+                            rank for rank in range(self.world_size) if rank != self.rank
+                        ],
+                        rank=self.rank,
+                        world_size=self.world_size,
+                        group=self.nccl_group,
+                        device_mesh=self.device_mesh,
+                    )
+                ],
+                # TODO: support qo comm meta calculation
+                num_remote_qo_tokens_per_stage=[0],
+                qo_group_collective_args_list=[None],  # type: ignore[list-item]
+            )
+
+            dist_attn_runtime = DistAttnRuntime(
+                comm_meta=comm_meta,
+                calc_meta=calc_meta,
+                cp_group_gc=self.nccl_groups[0],
+                cp_group_gr=self.nccl_groups[1],
+            )
+
+            local_q = torch.randn(
+                128, 8, 128, device=device, dtype=dtype, requires_grad=True
+            )
+            local_k = torch.randn(
+                128, 4, 128, device=device, dtype=dtype, requires_grad=True
+            )
+            local_v = torch.randn(
+                128, 4, 128, device=device, dtype=dtype, requires_grad=True
+            )
+            total_mask = torch.ones(512, 512, device=device).bool()
+            total_sink = (
+                torch.randn(
+                    seqlen_sink,
+                    8,
+                    device=device,
+                    dtype=torch.float32,
+                    requires_grad=True,
                 )
-            ],
-            # TODO: support qo comm meta calculation
-            num_remote_qo_tokens_per_stage=[0],
-            qo_group_collective_args_list=[None],  # type: ignore[list-item]
-        )
+                if seqlen_sink > 0
+                else None
+            )
 
-        dist_attn_runtime = DistAttnRuntime(
-            comm_meta=comm_meta,
-            calc_meta=calc_meta,
-            cp_group_gc=self.nccl_groups[0],
-            cp_group_gr=self.nccl_groups[1],
-        )
+            local_out, local_lse = dist_attn_func(
+                q=local_q,
+                k=local_k,
+                v=local_v,
+                dist_attn_runtime=dist_attn_runtime,
+                sink=total_sink,
+            )
+            total_out = torch.cat(all_gather(local_out, group=self.nccl_group), dim=0)
+            total_lse = torch.cat(all_gather(local_lse, group=self.nccl_group), dim=0)
 
-        local_q = torch.randn(
-            128, 1, 128, device=device, dtype=dtype, requires_grad=True
-        )
-        local_k = torch.randn(
-            128, 1, 128, device=device, dtype=dtype, requires_grad=True
-        )
-        local_v = torch.randn(
-            128, 1, 128, device=device, dtype=dtype, requires_grad=True
-        )
+            grad_total_out = torch.randn_like(total_out)
+            total_out.backward(grad_total_out)
+            local_grad_q, local_grad_k, local_grad_v = (
+                local_q.grad,
+                local_k.grad,
+                local_v.grad,
+            )
+            local_q.grad, local_k.grad, local_v.grad = None, None, None
+            if total_sink is not None:
+                total_dsink = total_sink.grad
+                total_sink.grad = None
+            else:
+                total_dsink = None
 
-        local_out, _ = dist_attn_func(local_q, local_k, local_v, dist_attn_runtime)
-        total_out = torch.cat(all_gather(local_out, group=self.nccl_group), dim=0)
+            total_q = torch.cat(all_gather(local_q, group=self.nccl_group), dim=0)
+            total_k = torch.cat(all_gather(local_k, group=self.nccl_group), dim=0)
+            total_v = torch.cat(all_gather(local_v, group=self.nccl_group), dim=0)
 
-        grad_total_out = torch.randn_like(total_out)
-        total_out.backward(grad_total_out)
-        local_grad_q, local_grad_k, local_grad_v = (
-            local_q.grad,
-            local_k.grad,
-            local_v.grad,
-        )
-        local_q.grad, local_k.grad, local_v.grad = None, None, None
+            total_out_ref, total_lse_ref = ref_attn_func(
+                q=total_q,
+                k=total_k,
+                v=total_v,
+                mask=total_mask,
+                sink=total_sink,
+                backend="torch" if total_sink is not None else "sdpa",
+                high_precision=True,
+                return_lse=True,
+            )
+            total_out_ref.backward(grad_total_out)
+            local_grad_q_ref, local_grad_k_ref, local_grad_v_ref = (
+                local_q.grad,
+                local_k.grad,
+                local_v.grad,
+            )
+            if total_sink is not None:
+                total_dsink_ref = total_sink.grad
+            else:
+                total_dsink_ref = None
 
-        total_q = torch.cat(all_gather(local_q, group=self.nccl_group), dim=0)
-        total_k = torch.cat(all_gather(local_k, group=self.nccl_group), dim=0)
-        total_v = torch.cat(all_gather(local_v, group=self.nccl_group), dim=0)
-
-        total_out_ref = scaled_dot_product_attention(
-            rearrange(total_q, "t h d -> 1 h t d"),
-            rearrange(total_k, "t h d -> 1 h t d"),
-            rearrange(total_v, "t h d -> 1 h t d"),
-            attn_mask=None,
-            dropout_p=0.0,
-            is_causal=False,
-        )
-        total_out_ref = rearrange(total_out_ref, "1 h t d -> t h d")
-        total_out_ref.backward(grad_total_out)
-        local_grad_q_ref, local_grad_k_ref, local_grad_v_ref = (
-            local_q.grad,
-            local_k.grad,
-            local_v.grad,
-        )
-        local_q.grad, local_k.grad, local_v.grad = None, None, None
-
-        torch.testing.assert_close(total_out, total_out_ref, atol=1e-2, rtol=1e-2)
-        torch.testing.assert_close(local_grad_q, local_grad_q_ref, atol=1e-2, rtol=1e-2)
-        torch.testing.assert_close(local_grad_k, local_grad_k_ref, atol=1e-2, rtol=1e-2)
-        torch.testing.assert_close(local_grad_v, local_grad_v_ref, atol=1e-2, rtol=1e-2)
+            assert_close(
+                total_out,
+                total_out_ref,
+                atol=EPSILON,
+                rtol=5e-2,
+                mismatch_threshold=0.05,
+                test_case="out",
+            )
+            assert_close(
+                total_lse,
+                total_lse_ref,
+                atol=EPSILON,
+                rtol=5e-3,
+                mismatch_threshold=0.01,
+                test_case="lse",
+            )
+            assert_close(
+                local_grad_q,
+                local_grad_q_ref,
+                atol=EPSILON,
+                rtol=5e-2,
+                mismatch_threshold=0.05,
+                test_case="dq",
+            )
+            assert_close(
+                local_grad_k,
+                local_grad_k_ref,
+                atol=EPSILON,
+                rtol=5e-2,
+                mismatch_threshold=0.05,
+                test_case="dk",
+            )
+            assert_close(
+                local_grad_v,
+                local_grad_v_ref,
+                atol=EPSILON,
+                rtol=5e-2,
+                mismatch_threshold=0.05,
+                test_case="dv",
+            )
+            if total_sink is not None:
+                assert_close(
+                    total_dsink,
+                    total_dsink_ref,
+                    atol=EPSILON,
+                    rtol=5e-3,
+                    mismatch_threshold=0.01,
+                    test_case="dsink",
+                )
 
 
 if __name__ == "__main__":
