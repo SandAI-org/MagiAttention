29c55a008a8fcbda00bd2ace94254cddbae5f404
WT1W <757539294@qq.com>
2025-11-25 08:39:59 +0000

    add check for xml

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -25,6 +25,8 @@ module ExternalPosts
     def fetch_from_rss(site, src)
       xml = HTTParty.get(src['rss_url']).body
       return if xml.nil?
+      puts "[DEBUG] First 200 chars of RSS from:"
+      puts xml[0..200]
       feed = Feedjira.parse(xml)
       process_entries(site, src, feed.entries)
     end
