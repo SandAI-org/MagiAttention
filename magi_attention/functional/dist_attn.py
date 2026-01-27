ad597a4e9ce6dbdb8698d32eb0ef65195558c09f
Strivin0311 <hyp@smail.nju.edu.cn>
2026-01-27 04:37:31 +0000

    removed debug gpu-cpu sync

--- magi_attention/functional/dist_attn.py
+++ magi_attention/functional/dist_attn.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+
 # mypy: disable-error-code="union-attr,list-item"
 import logging
 import warnings
