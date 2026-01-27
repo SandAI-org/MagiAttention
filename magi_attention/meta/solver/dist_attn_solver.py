06ac728ea27417169bce56a441f90f4da486454e
WT1W <757539294@qq.com>
2026-01-27 11:18:12 +0000

    Modify the static solver so that each segment of input_split_size is divisible by the same number

--- magi_attention/meta/solver/dist_attn_solver.py
+++ magi_attention/meta/solver/dist_attn_solver.py
@@ -24,6 +24,7 @@ import torch.distributed as dist
 from torch.distributed.device_mesh import DeviceMesh
 
 import magi_attention
+from magi_attention.comm import native_grpcoll_split_alignment
 from magi_attention.comm.primitive.grpcoll.utils import (
     sanity_check_for_group_cast_meta_args_per_rank,
 )
@@ -364,6 +365,28 @@ class DistAttnSolver(BaseDistAttnSolver):
                 )
             )
 
+            if native_grpcoll_split_alignment() > 1:
+                split_aligment = native_grpcoll_split_alignment()
+                assert dispatch_meta_q.chunk_size % split_aligment == 0, (
+                    f"chunk_size % split_alignment must be zero,"
+                    f"but got chunk_size={dispatch_meta_q.chunk_size}, split_aligment={split_aligment}."
+                )
+                for i, attn_range in enumerate(remote_k_ranges_global_this_rank):
+                    if (
+                        attn_range.start % split_aligment != 0
+                        or attn_range.end % split_aligment != 0
+                    ):
+                        remote_k_ranges_global_this_rank[i] = AttnRange(
+                            start=(attn_range.start // split_aligment) * split_aligment,
+                            end=(
+                                (attn_range.end + split_aligment - 1) // split_aligment
+                            )
+                            * split_aligment,
+                        )
+                remote_k_ranges_global_this_rank = (
+                    remote_k_ranges_global_this_rank.merge()
+                )
+
         # sanity check
         if magi_attention.is_sanity_check_enable():
             # check if merged successfully
@@ -517,19 +540,31 @@ class DistAttnSolver(BaseDistAttnSolver):
             self.overlap_chunk_size = 0
             remote_k_ranges_global_per_chunk: list[AttnRanges] = []  # empty list
         else:
-            # determine the chunk size constrainted by min_chunk_size and max_num_chunks
-            total_remote_k_seqlen = remote_k_ranges_global.total_seqlen
-            num_chunks = (
-                total_remote_k_seqlen + self.overlap_config.min_chunk_size - 1
-            ) // self.overlap_config.min_chunk_size
-            if num_chunks <= self.overlap_config.max_num_chunks:
-                self.overlap_chunk_size = self.overlap_config.min_chunk_size
-                self.overlap_num_chunks = num_chunks
+            if native_grpcoll_split_alignment() == 1:
+                # determine the chunk size constrainted by min_chunk_size and max_num_chunks
+                total_remote_k_seqlen = remote_k_ranges_global.total_seqlen
+                num_chunks = (
+                    total_remote_k_seqlen + self.overlap_config.min_chunk_size - 1
+                ) // self.overlap_config.min_chunk_size
+                if num_chunks <= self.overlap_config.max_num_chunks:
+                    self.overlap_chunk_size = self.overlap_config.min_chunk_size
+                    self.overlap_num_chunks = num_chunks
+                else:
+                    self.overlap_num_chunks = self.overlap_config.max_num_chunks
+                    self.overlap_chunk_size = (
+                        total_remote_k_seqlen + self.overlap_num_chunks - 1
+                    ) // self.overlap_num_chunks
+                    self.overlap_num_chunks = (
+                        total_remote_k_seqlen + self.overlap_chunk_size - 1
+                    ) // self.overlap_chunk_size
             else:
-                self.overlap_num_chunks = self.overlap_config.max_num_chunks
+                split_aligment = native_grpcoll_split_alignment()
+                total_remote_k_seqlen = remote_k_ranges_global.total_seqlen
+
                 self.overlap_chunk_size = (
-                    total_remote_k_seqlen + self.overlap_num_chunks - 1
-                ) // self.overlap_num_chunks
+                    (self.overlap_config.min_chunk_size + split_aligment - 1)
+                    // split_aligment
+                ) * split_aligment
                 self.overlap_num_chunks = (
                     total_remote_k_seqlen + self.overlap_chunk_size - 1
                 ) // self.overlap_chunk_size
