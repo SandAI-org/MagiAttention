837be496b36ab35e18c83fe106ee5abd3367df40
Strivin0311 <hyp@smail.nju.edu.cn>
2026-02-11 16:59:23 +0000

    fixed lint

--- magi_attention/functional/dist_attn.py
+++ magi_attention/functional/dist_attn.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-
 # mypy: disable-error-code="union-attr,list-item"
 import logging
 import warnings
